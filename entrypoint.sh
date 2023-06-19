83879149396071ca31fbf71474fd8904947cc8d0
deep-soft <deep-soft@users.noreply.github.com>
2023-06-19 21:37:12 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,8 @@
 # 2023-06-18 20:30
 
 # Create sha sums or exit if command fails
-#set -euf
+#set -eu
+set -f
 if [ "$DEBUG_MODE" = "yes" ]
 then
   set -x
@@ -17,21 +18,23 @@ fi
 
 if [ "$INPUT_TYPE" = "sha1" ] || [ "$INPUT_TYPE" = "sha256" ] || [ "$INPUT_TYPE" = "sha512" ] || [ "$INPUT_TYPE" = "md5" ] 
 then
+  EXCLUSIONS=""
   if [ "$RUNNER_OS" = "Windows" ]
   then
-    EXCLUSIONS=" -x!"
-    EXCLUSIONS+="$INPUT_FILENAME"
+    if [ "$IGNORE_GIT" = "true" ]
+    then
+      EXCLUSIONS+=" -x!./.git/ -x!*.git/ -x!.git/"
+    fi
+    EXCLUSIONS+=" -x!$INPUT_FILENAME"
     if [ -n "$INPUT_EXCLUSIONS" ] || [ -n "$INPUT_RECURSIVE_EXCLUSIONS" ]
     then 
       for EXCLUSION in $INPUT_EXCLUSIONS
       do
-        EXCLUSIONS+=" -x!"
-        EXCLUSIONS+="$EXCLUSION"
+        EXCLUSIONS+=" -x!$EXCLUSION"
       done
       for EXCLUSION in $INPUT_RECURSIVE_EXCLUSIONS
       do
-        EXCLUSIONS+=" -xr!"
-        EXCLUSIONS+="$EXCLUSION"
+        EXCLUSIONS+=" -xr!$EXCLUSION"
       done
     fi
     # 7z h -scrcSHA256 -xr!LICENSE -x!_sha_sums_tmp_ * >_sha_sums_tmp_
@@ -42,30 +45,36 @@ then
     7z h -scrc"$INPUT_TYPE" -ba $INPUT_PATH $EXCLUSIONS | grep -v "\\[ ]$" | awk "{printf (\"%s  %s\n\", \$1, \$3)}" > "$INPUT_FILENAME" || { printf "\n⛔ Unable to create %s file.\n" "$INPUT_TYPE"; exit 1;  }
   elif [ "$RUNNER_OS" = "macOS" ]
   then
-    EXCLUSIONS=" -not -name $INPUT_FILENAME"
+    if [ "$IGNORE_GIT" = "true" ]
+    then
+      EXCLUSIONS+=" -not -path ./.git/ -not -path .git/"
+    fi
+    EXCLUSIONS+=" -not -name $INPUT_FILENAME"
     if [ -n "$INPUT_EXCLUSIONS" ]
     then
       for EXCLUSION in $INPUT_EXCLUSIONS
       do
-        EXCLUSIONS+=" -not -name "
-        EXCLUSIONS+="$EXCLUSION"
+        EXCLUSIONS+=" -not -name $EXCLUSION"
       done
     fi
     # work on windows but not in GH CI # 7z h -scrc$INPUT_TYPE -ba $INPUT_PATH $EXCLUSIONS | awk "OFS=\"\\t\" {printf (\"%s  %s\n\", $1, $3)}" | grep -v "\\ $" > $INPUT_FILENAME
-    # INPUT_TYPE_L=${INPUT_TYPE,,}
+    # lowercase not working in macOS INPUT_TYPE_L=${INPUT_TYPE,,}
     find $INPUT_PATH -type f $EXCLUSIONS -exec shasum -a ${INPUT_TYPE#???} {} \; > $INPUT_FILENAME || { printf "\n⛔ Unable to create %s file.\n" "$INPUT_TYPE"; exit 1;  }
   else
-    EXCLUSIONS=" -not -name $INPUT_FILENAME"
+    if [ "$IGNORE_GIT" = "true" ]
+    then
+      EXCLUSIONS+=" -not -path ./.git/ -not -path .git/"
+    fi
+    EXCLUSIONS+=" -not -name $INPUT_FILENAME"
     if [ -n "$INPUT_EXCLUSIONS" ]
     then
       for EXCLUSION in $INPUT_EXCLUSIONS
       do
-        EXCLUSIONS+=" -not -name "
-        EXCLUSIONS+="$EXCLUSION"
+        EXCLUSIONS+=" -not -name $EXCLUSION"
       done
     fi
     # 7z h -scrc$INPUT_TYPE -ba $INPUT_PATH $EXCLUSIONS | awk "OFS=\"\\t\" {printf (\"%s  %s\n\", $1, $3)}" | grep -v "\\ $" > $INPUT_FILENAME
-    # INPUT_TYPE_L=${INPUT_TYPE,,}
+    # lowercase not working in macOS INPUT_TYPE_L=${INPUT_TYPE,,}
     find $INPUT_PATH -type f $EXCLUSIONS -exec "$INPUT_TYPE"sum {} \; > $INPUT_FILENAME || { printf "\n⛔ Unable to create %s file.\n" "$INPUT_TYPE"; exit 1;  }
   fi
 else
